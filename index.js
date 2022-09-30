a8ba225bd84a5f2b60430d58c928d9ce9eab0949
Franco Marcellini <francomarcellini@gmail.com>
2022-09-29 22:00:23 -0300

    agregando DOM  y EVENTOS

--- index.js
+++ index.js
@@ -1,37 +1,45 @@
+let precioPosavaso = 1600;
+let buyGrid = document.getElementById("itemsCompra");
+let carritoDeCompras = document.getElementById("carritoDeCompras");
+let productos = [
+    {
+        id: 1,
+        nombre: "Posa vaso Jigglypuf",
+        precio : precioPosavaso,
+        imagen :"/resources/A1.jpg",
+    },
+    {
+        id: 2,
+        nombre: "Posa vaso Scyther",
+        precio : precioPosavaso,
+        imagen :"/resources/B1.jpg",
+    },
+    {
+        id: 3,
+        nombre: "Posa vaso Gastly",
+        precio : precioPosavaso,
+        imagen :"/resources/C1.jpg",
+    }
+];
+let carrito = []
+productos.forEach(productos=>{
+    let items = document.createElement("div")
+    items.innerHTML=`<div class="card" style="width: 18rem;">
+    <img src=${productos.imagen} class="card-img-top" alt="...">
+    <div class="card-body">
+        <h5 class="card-title">${productos.nombre}</h5>
+        <h5 class="card-title">$${productos.precio}</h5>
+        <button id=${productos.id} href="#" class="btn btn-primary">COMPRAR</button>
+        <button href="#" class="btn btn-primary">ELIMINAR</button>
+    </div>
+    </div>`
+    buyGrid.append(items);
+    const button =document.getElementById(productos.id)
+    button.addEventListener("click", () => agregarCarrito(productos))
+})
 
-
-
-/* alert("Comprar"); */
-
-function Produto(nombre,cantidad,precio){
-    this.nombre = nombre
-    this.cantidad = cantidad
-    this.precio = precio
+const agregarCarrito = (productos) => {
+    carrito.push(productos) 
 }
-const producto1 = new Produto ("Posavasos",2,1600);
-const producto2 = new Produto ("Bandeja",1,1800);
-const producto3 = new Produto ("Soporte Celular",1,2000);
-const totalBruto = ((producto1.cantidad*producto1.precio)+(producto2.cantidad+producto2.precio)+(producto3.cantidad*producto3.precio));
 
-const carrito = [producto1,producto2,producto3];
-
-alert("agregaste "+producto1.nombre+" por un total de "+producto1.cantidad * producto1.precio);
-alert("agregaste "+producto2.nombre+" por un total de "+producto2.cantidad * producto2.precio);
-alert("agregaste "+producto3.nombre+" por un total de "+producto3.cantidad * producto3.precio);
-alert("tu total es de "+ totalBruto);
-
-let suma = (a,b) => a + b;
-let iva = x => x * 0.21;
-let precioProducto =totalBruto;
-let nuevoPrecio = suma(precioProducto, iva(precioProducto));
-
-if (nuevoPrecio > 3800) {
-    alert("el total es" +" " +nuevoPrecio);
-    alert("3 cuotas sin interes disponibles por tu compra mayor a 3800 :D" )
-    cuotasSinInt = (nuevoPrecio / 3);
-    alert("3 cuotas sin interes de " +" "+ cuotasSinInt );
-} else {
-    alert("cuotas sin interes solo disponibles en compras superior a $ 3800")
-    alert("el total es" +" " +nuevoPrecio)
-}
-
+carritoDeCompras.addEventListener("click", () => console.log(carrito))
