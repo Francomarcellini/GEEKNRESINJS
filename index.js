f04717e95f87c3685c1c5214d44403bb976ded56
Franco Marcellini <francomarcellini@gmail.com>
2022-10-04 20:58:52 -0300

    AGREGANDO FUNCIONALIDAD A LOS BOTONES

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let precioPosavaso = 1600;
+let precioPosavaso = 1900;
 let buyGrid = document.getElementById("itemsCompra");
 let carritoDeCompras = document.getElementById("carritoDeCompras");
 let productos = [
@@ -19,7 +19,13 @@ let productos = [
         nombre: "Posa vaso Gastly",
         precio : precioPosavaso,
         imagen :"/resources/C1.jpg",
-    }
+    },
+    {
+        id: 4,
+        nombre: "Posa vaso Alakazam",
+        precio : precioPosavaso,
+        imagen :"/resources/D1.jpg",
+    },
 ];
 let carrito = []
 productos.forEach(productos=>{
@@ -30,16 +36,25 @@ productos.forEach(productos=>{
         <h5 class="card-title">${productos.nombre}</h5>
         <h5 class="card-title">$${productos.precio}</h5>
         <button id=${productos.id} href="#" class="btn btn-primary">COMPRAR</button>
-        <button href="#" class="btn btn-primary">ELIMINAR</button>
+        <button id="borrarProducto${productos.id}" href="#" class="btn btn-primary">ELIMINAR</button>
     </div>
     </div>`
     buyGrid.append(items);
     const button =document.getElementById(productos.id)
+    const borrarButton =document.getElementById(`borrarProducto${productos.id}`)
+    borrarButton.addEventListener("click", () => quitarCarrito(productos))
     button.addEventListener("click", () => agregarCarrito(productos))
 })
 
 const agregarCarrito = (productos) => {
     carrito.push(productos) 
 }
+const quitarCarrito = (productos) => {
+    carrito.shift(productos)
+}
+carritoDeCompras.addEventListener("click", () => console.log(carrito));
+carritoDeCompras.addEventListener("click", () => localStorage.setItem("carrito",JSON.stringify(carrito)));
 
-carritoDeCompras.addEventListener("click", () => console.log(carrito))
+
+
+
